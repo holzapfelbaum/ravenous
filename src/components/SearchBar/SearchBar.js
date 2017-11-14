9b36af205cea9127de876655082b86745c3ba346
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-13 22:47:22 -0800

    Added SearchBar component to components directory in src directory

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+  renderSortByOptions() { // purpose of renderSortByOptions() is to dynamically create the list items needed to display the sort options (Best Match, Highest Rated, Most Reviewed), to help future proof against potential changes to the Yelp API.
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption]; // Inside of the callback function, access the sortByOptions values using the sortByOption parameter of the callback function. Store values in variable called sortByOptionValue.
+      return <li key={this.sortByOptionValue}>{this.sortByOption}</li>;
+    });
+  }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
+  };
+}
+
+export default SearchBar;
