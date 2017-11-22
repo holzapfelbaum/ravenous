04288d84919462b3391d973d7ce24195ef1247f8
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-21 21:52:32 -0800

    Created methods to handle both a term (business) or location change and onChange attributes in render method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,21 +11,31 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
     term: '';
     location: '';
     sortBy: 'best_match';
   }
 
   getSortByClass(sortByOption) {
-    if(this.state.sortBy === sortByOption) {
+    if(this.state.sortBy == sortByOption) {
       return 'active';
     } else {
       return '';
-    }
+    };
   }
 
   handleSortByChange(sortByOption) {
     this.setState({sortBy: sortByOption})
+  }
+
+  handleTermChange(event) {
+    this.setState({term: event.target.value})  // Inside of both methods, the state of each input element should be updated to reflect the text typed into the respective input element
+  }
+
+  handleLocationChange(event) {
+    this.setState({location: event.target.value})
   }
 
   renderSortByOptions() { // purpose of renderSortByOptions() is to dynamically create the list items needed to display the sort options (Best Match, Highest Rated, Most Reviewed), to help future proof against potential changes to the Yelp API.
@@ -44,8 +54,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
           <a>Let's Go</a>
