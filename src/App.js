cccf3482daae92d6646533297f0635083cfb7def
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-26 21:54:28 -0800

    Modified App.js to remove hardcoded information from business and businesses variables, add constructor function in App component class, and modify the searchYelp method

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import './App.css';
 // import logo from './logo.svg';
 import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
+import Yelp from './util/Yelp.js';
 
 const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
+  imageSrc: ,
+  name: ,
+  address: ,
+  city: ,
+  state: ,
+  zipCode: ,
+  category: ,
+  rating: ,
+  reviewCount:
 }
 
 const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
+
 ];
 
 class App extends Component {
+  constructor() {
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log('I am sending a request to Yelp with ' + term + ', ' + location + ', ' + sortBy + '.')
+    Yelp.search(term, location, sortBy).then(businesses => {
+      setState({businesses: businesses});
+    });
   }
 
   render() {
