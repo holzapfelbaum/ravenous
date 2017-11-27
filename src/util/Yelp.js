dcbbe01b9ba6a066d18fb1c9b5db15387e764034
Jonathan Crabtree <jonathancrabtree@Jonathans-iMac.local>
2017-11-26 22:10:44 -0800

    Changed Yelp.js to fix syntax errors shown in console in live app

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -6,7 +6,7 @@ export const Yelp = {
   getAccessToken() {
     if (accessToken) {
       return new Promise(resolve => {
-        resolve(accessToken));
+        resolve(accessToken);
       });
     }
     return fetch(`https://cors-anywhere.herokuapp.com/ + https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id= + ${clientId} + &client_secret= + ${secret}`, {
@@ -19,29 +19,29 @@ export const Yelp = {
   },
 
   search(term, location, sortBy) {
-    return Yelp.getAccessToken().then(function() => {
+    return Yelp.getAccessToken().then(() => {
       return fetch(`https://cors-anywhere.herokuapp.com/ + https://api.yelp.com/v3/businesses/search?term= + ${term} + &location= + ${location} + &sort_by= ${sortBy}`)
     }, {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
-    });
-  }).then(response => {
-    return response.json();
-  }).then(jsonResponse => {
-    if (jsonResponse.businesses) {
-      return jsonResponse.businesses.map(business => {
-        id: business.id,
-        imageSrc: business.image_url,
-        name: business.name,
-        address: business.location.address1,
-        city: business.location.city,
-        state: business.location.state,
-        zipCode: business.location.zip_code,
-        category: business.categories,
-        rating: business.rating,
-        reviewCount: business.review_count
-      });
-    }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories,
+          rating: business.rating,
+          reviewCount: business.review_count
+        });
+      };
+  })
   })
 }
